9d934557ef342e511341bd3a62eb5cb09a3eed57
gaganphp <mailtogagan990@gmail.com>
2015-06-05 18:54:17 +0530

    fixed issues
    - fixed API manager code
    - fixed theme setting page title from AM theme to SP theme
    - fixed setting tab styles

--- sp-framework/am/function.php
+++ sp-framework/am/function.php
@@ -159,7 +159,7 @@ class API_Manager_theme_Eggo {
 			$this->ame_deactivate_checkbox 			= 'am_deactivate_eggo_checkbox';
 			$this->ame_activation_tab_key 			= 'api_manager_theme_eggo_dashboard';
 			$this->ame_deactivation_tab_key 		= 'api_manager_theme_eggo_deactivation';
-			$this->ame_settings_menu_title 			= 'AM Theme Eggo';
+			$this->ame_settings_menu_title 			= 'SP Theme Eggo';
 			$this->ame_settings_title 				= 'API Manager Theme Eggo';
 			$this->ame_menu_tab_activation_title 	= __( 'License Activation', 'api-manager-eggo' );
 			$this->ame_menu_tab_deactivation_title 	= __( 'License Deactivation', 'api-manager-eggo' );
